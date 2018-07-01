a8e3ab8db5ac905de2010c12c7761a5797efc257
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-07-01 16:43:11 +0430

    save contact form

--- content/contact/model.php
+++ content/contact/model.php
@@ -1,9 +1,7 @@
 <?php
 namespace content\contact;
-use \lib\debug;
-use \lib\utility;
 
-class model extends \mvc\model
+class model
 {
 
 	// log callers
@@ -17,75 +15,81 @@ class model extends \mvc\model
 	/**
 	 * save contact form
 	 */
-	public function post_contact()
+	public static function post()
 	{
 		// check login
-		if($this->login())
+		if(\dash\user::login())
 		{
-			$user_id = $this->login("id");
+			$user_id = \dash\user::id();
 
 			// get mobile from user login session
-			$mobile = $this->login('mobile');
+			$mobile = \dash\user::login('mobile');
 
 			if(!$mobile)
 			{
-				$mobile = utility::post('mobile');
+				$mobile = \dash\request::post('mobile');
 			}
 
 			// get display name from user login session
-			$displayname = $this->login("displayname");
+			$displayname = \dash\user::login("displayname");
 			// user not set users display name, we get display name from contact form
 			if(!$displayname)
 			{
-				$displayname = utility::post("name");
+				$displayname = \dash\request::post("name");
 			}
 			// get email from user login session
-			$email = \lib\db\users::get_email($user_id);
+			$email = \dash\db\users::get_email($user_id);
 			// user not set users email, we get email from contact form
 			if(!$email)
 			{
-				$email = utility::post("email");
+				$email = \dash\request::post("email");
 			}
 		}
 		else
 		{
 			// users not registered
 			$user_id     = null;
-			$displayname = utility::post("name");
-			$email       = utility::post("email");
-			$mobile      = utility::post("mobile");
+			$displayname = \dash\request::post("name");
+			$email       = \dash\request::post("email");
+			$mobile      = \dash\request::post("mobile");
 		}
 		// get the content
-		$content = utility::post("content");
+		$content = \dash\request::post("content");
 
 		// save log meta
 		$log_meta =
 		[
 			'meta' =>
 			[
-				'login'    => $this->login('all'),
-				'language' => \lib\define::get_language(),
-				'post'     => utility::post(),
+				'login'    => \dash\user::login('all'),
+				'language' => \dash\language::current(),
+				'post'     => \dash\request::post(),
 			]
 		];
 
 		/**
 		 * register user if set mobile and not register
 		 */
-		if($mobile && !$this->login())
+		if($mobile && !\dash\user::login())
 		{
+			$mobile = \dash\utility\filter::mobile($mobile);
+
 			// check valid mobile
-			if(\lib\utility\filter::mobile($mobile))
+			if($mobile)
 			{
 				// check existing mobile
-				$exists_user = \lib\db\users::get_by_mobile($mobile);
+				$exists_user = \dash\db\users::get_by_mobile($mobile);
 				// register if the mobile is valid
 				if(!$exists_user || empty($exists_user))
 				{
 					// signup user by site_guest
-					$user_id = \lib\db\users::signup(['mobile' => $mobile ,'type' => 'inspection', 'port' => 'site_guest']);
+					$user_id = \dash\db\users::signup(['mobile' => $mobile, 'displayname' => $displayname]);
 					// save log by caller 'user:send:contact:register:by:mobile'
-					\lib\db\logs::set('user:send:contact:register:by:mobile', $user_id, $log_meta);
+					\dash\db\logs::set('user:send:contact:register:by:mobile', $user_id, $log_meta);
+				}
+				elseif(isset($exists_user['id']))
+				{
+					$user_id = $exists_user['id'];
 				}
 			}
 		}
@@ -93,40 +97,37 @@ class model extends \mvc\model
 		// check content
 		if($content == '')
 		{
-			\lib\db\logs::set('user:send:contact:empty:message', $user_id, $log_meta);
-			debug::error(T_("Please fill in the field"), "content");
+			\dash\notif::error(T_("Please try type something!"), "content");
 			return false;
 		}
 		// ready to insert comments
 		$args =
 		[
-			'comment_author'  => $displayname,
-			'comment_email'   => $email,
-			'comment_type'    => 'comment',
-			'comment_content' => $content,
-			'user_id'         => $user_id
+			'author'  => $displayname,
+			'email'   => $email,
+			'type'    => 'comment',
+			'content' => $content,
+			'user_id' => $user_id
 		];
 		// insert comments
-		$result = \lib\db\comments::insert($args);
+		$result = \dash\db\comments::insert($args);
 		if($result)
 		{
 			// $mail =
 			// [
-			// 	'from'    => 'info@sarshomar.com',
 			// 	'to'      => 'info@sarshomar.com',
 			// 	'subject' => 'contact',
 			// 	'body'    => $content,
-			// 	'debug'   => false,
 			// ];
-			// \lib\utility\mail::send($mail);
+			// \dash\mail::send($mail);
 
-			\lib\db\logs::set('user:send:contact', $user_id, $log_meta);
-			debug::true(T_("Thank You For contacting us"));
+			\dash\db\logs::set('user:send:contact', $user_id, $log_meta);
+			\dash\notif::ok(T_("Thank You For contacting us"));
 		}
 		else
 		{
-			\lib\db\logs::set('user:send:contact:fail', $user_id, $log_meta);
-			debug::error(T_("We could'nt save the contact"));
+			\dash\db\logs::set('user:send:contact:fail', $user_id, $log_meta);
+			\dash\notif::error(T_("We could'nt save the contact"));
 		}
 	}
 }
