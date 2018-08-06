bec592b4a90bdb9df2207414bdd665fc18a4b5be
Reza <rm.biqarar@gmail.com>
2018-08-06 13:05:43 +0430

    add page step in edit poll

--- content_a/poll/general/view.php
+++ content_a/poll/general/view.php
@@ -6,11 +6,14 @@ class view
 {
 	public static function config()
 	{
+		\dash\data::page_pictogram('edit');
 
-		\dash\data::page_pictogram('plus');
+		\content_a\poll\view::load();
 
-		\dash\data::page_title(T_("Add new poll"));
-		\dash\data::page_desc(T_("add new poll by some data and can edit it later"));
+		\dash\data::page_title(T_("Edit poll"). ' | '. \dash\data::dataRow_title());
+
+		\dash\data::page_desc(T_("You can edit your poll detail"));
+
 		\dash\data::badge_link(\dash\url::this());
 		\dash\data::badge_text(T_('Back to poll list'));
 
---
+++ content_a/poll/view.php
@@ -0,0 +1,21 @@
+<?php
+namespace content_a\poll;
+
+
+class view
+{
+	public static function load()
+	{
+		$id = \dash\request::get('id');
+		$load = \lib\app\poll::get($id);
+		if(!$load)
+		{
+			\dash\header::status(404, T_("Invalid poll id"));
+		}
+
+		\dash\data::dataRow($load);
+
+		return $load;
+	}
+}
+?>
