e0df7aa3c9b20d07ba0db80f64c9405bf5274a04
Reza <rm.biqarar@gmail.com>
2018-08-07 13:20:03 +0430

    show list of poll

--- content_a/poll/home/view.php
+++ content_a/poll/home/view.php
@@ -9,6 +9,11 @@ class view
 		\dash\data::page_pictogram('magic');
 		\dash\data::page_title(T_("Poll list"));
 		\dash\data::page_desc(T_("check last poll and add or edit a poll"));
+
+		$arg = [];
+		$arg['user_id'] = \dash\user::id();
+		$dataTable = \lib\app\poll::list(null, $arg);
+		\dash\data::dataTable($dataTable);
 	}
 }
 ?>
