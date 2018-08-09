bafdda8730c21d4dd810a0fe7b5a115a80caf025
Reza <rm.biqarar@gmail.com>
2018-08-09 16:30:09 +0430

    add settings module

---
+++ content_a/question/settings/model.php
@@ -0,0 +1,22 @@
+<?php
+namespace content_a\question\settings;
+
+
+class model
+{
+	public static function post()
+	{
+		$post            = [];
+		$post['title']   = \dash\request::post('title');
+		$post['desc']    = \dash\request::post('desc');
+		$post['poll_id'] = \dash\request::get('id');
+
+		$result = \lib\app\question::edit($post, \dash\request::get('questionid'));
+
+		if(\dash\engine\process::status())
+		{
+			\dash\redirect::pwd();
+		}
+	}
+}
+?>
