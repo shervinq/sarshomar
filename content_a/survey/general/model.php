14d03f5a02b07c624587e2bb213462a931bd6822
Reza <rm.biqarar@gmail.com>
2018-08-13 17:49:35 +0430

    save language

--- content_a/survey/general/model.php
+++ content_a/survey/general/model.php
@@ -7,8 +7,9 @@ class model
 	public static function post()
 	{
 		$post           = [];
-		$post['title']  = \dash\request::post('title');
-		$post['desc']   = \dash\request::post('desc');
+		$post['title']    = \dash\request::post('title');
+		$post['desc']     = \dash\request::post('desc');
+		$post['language'] = \dash\request::post('language');
 
 		$result = \lib\app\survey::edit($post, \dash\request::get('id'));
 
