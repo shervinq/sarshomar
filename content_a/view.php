27e27fd711a6989092ddd4e688c23cf499f1ef6a
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-13 16:24:32 +0430

    add branding to panel

--- content_a/view.php
+++ content_a/view.php
@@ -17,12 +17,16 @@ class view
 		\dash\data::display_question('content_a/question/layout.html');
 		\dash\data::display_admin('content_a/main/layout.html');
 
-		// \dash\data::template_social('content/template/social.html');
-		// \dash\data::template_share('content/template/share.html');
 		\dash\data::include_adminPanel(true);
 		\dash\data::include_css(false);
 		\dash\data::include_js(false);
-		// \dash\data::bodyclass('fixed unselectable');
+
+
+		\dash\data::service_title(T_("Sarshomar"));
+		\dash\data::service_desc(T_("Focus on your question. Do not be too concerned about how to ask or analyze."));
+		\dash\data::service_slogan(T_("Ask Anyone Anywhere"));
+		\dash\data::service_logo(\dash\url::site(). '/static/siftal/images/logo/sarshomar.png');
+		\dash\data::service_url('https://sarshomar.com');
 	}
 }
 ?>
