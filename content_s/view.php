871d6a2b2242234f20b768bc15e8a6c7581c0def
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-13 14:31:46 +0430

    add some detail to sidebar

--- content_s/view.php
+++ content_s/view.php
@@ -11,18 +11,10 @@ class view
 		\dash\data::site_slogan(T_("Ask Anyone Anywhere"));
 		\dash\data::page_desc(\dash\data::site_desc(). ' | '. \dash\data::site_slogan());
 
-		// for pushstate of main page
-		\dash\data::template_xhr('content/main/layout-xhr.html');
-		\dash\data::display_survey('content_a/survey/layout.html');
-		\dash\data::display_question('content_a/question/layout.html');
 
-		\dash\data::template_social('content/template/social.html');
-		\dash\data::template_share('content/template/share.html');
 		\dash\data::include_adminPanel(true);
 		\dash\data::include_css(false);
 		\dash\data::include_js(false);
-		\dash\data::bodyclass('fixed unselectable');
-
 
 
 		\dash\data::service_title(T_("Sarshomar"));
