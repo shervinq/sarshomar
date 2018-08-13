c078676f58b6b6a77cbf11da18df9cd077e32653
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-08-13 16:25:41 +0430

    fix url of branding

--- content_a/view.php
+++ content_a/view.php
@@ -26,7 +26,7 @@ class view
 		\dash\data::service_desc(T_("Focus on your question. Do not be too concerned about how to ask or analyze."));
 		\dash\data::service_slogan(T_("Ask Anyone Anywhere"));
 		\dash\data::service_logo(\dash\url::site(). '/static/siftal/images/logo/sarshomar.png');
-		\dash\data::service_url('https://sarshomar.com');
+		\dash\data::service_url(\dash\url::kingdom());
 	}
 }
 ?>
