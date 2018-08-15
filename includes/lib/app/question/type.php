4978fb2f1f9c484f1171da352ca7f5a0eb2c1d24
Reza <rm.biqarar@gmail.com>
2018-08-15 16:38:53 +0430

    fix type list

--- includes/lib/app/question/type.php
+++ includes/lib/app/question/type.php
@@ -38,17 +38,20 @@ trait type
 		// $sample =
 		// [
 		// 	'key'           => 'picture_choice',
+		//  'placeholder'   => true,
 		// 	'title'         => T_('Picture choice'),
 		// 	'choice'        => true,
 		// 	'random'        => true,
-		// 	'min'        => false,
-		// 	'max'        => false,
+		// 	'min'           => false,
+		// 	'max'           => false,
 		// 	'otherchoice'   => false,
 		// 	'chart'         => false,
 		// 	'prifile_field' => null,
 		// 	'maxchar'       => false,
+		// 	'minchoice'     => false,
+		// 	'maxchoice'     => false,
 		// 	'desc'          => T_('Picture choice'),
-		// 	'logo'			=> \dash\url::site(). '/static/images/logo.png',
+		// 	'logo'          => \dash\url::site(). '/static/images/logo.png',
 		// 	'upload_choice' => true,
 		// ];
 
@@ -58,13 +61,16 @@ trait type
 		[
 			'key'           => 'short_answer',
 			'title'         => T_("Short answer"),
+			'placeholder'   => true,
 			'maxchar'       => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
 
+
 		$type['descriptive_answer'] =
 		[
 			'key'           => 'descriptive_answer',
+			'placeholder'   => true,
 			'title'         => T_('Descriptive answer'),
 			'maxchar'       => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
@@ -74,11 +80,13 @@ trait type
 		$type['numeric'] =
 		[
 			'key'           => 'numeric',
+			'placeholder'   => true,
 			'title'         => T_('Numberic'),
 			'min'           => true,
 			'max'           => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
+
 
 		$type['single_choice'] =
 		[
@@ -87,10 +95,9 @@ trait type
 			'choice'        => true,
 			'random'        => true,
 			'chart'         => true,
-			'otherchoice'   => true,
-			'maxchar'       => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
+
 
 		$type['multiple_choice'] =
 		[
@@ -99,89 +106,54 @@ trait type
 			'choice'        => true,
 			'random'        => true,
 			'chart'         => true,
-			'otherchoice'   => true,
-			'maxchar'       => true,
+			'minchoice'     => true,
+			'maxchoice'     => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
+
 
 		$type['dropdown'] =
 		[
 			'key'           => 'dropdown',
 			'title'         => T_('Dropdown'),
+			'placeholder'   => true,
 			'choice'        => true,
 			'chart'         => true,
 			'random'        => true,
-			'otherchoice'   => true,
-			'maxchar'       => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
 
-
-		// $type['card_descign'] =
-		// [
-		// 	'key'           => 'card_descign',
-		// 	'title'         => T_('Card design'),
-		// 	'choice'        => true,
-		// 	'random'        => true,
-		// 	'logo'			=> \dash\url::site(). '/static/images/logo.png',
-		// 	'upload_choice' => true,
-		// ];
-
-
-		$type['confirm'] =
-		[
-			'key'           => 'confirm',
-			'title'         => T_('Confirm buttom'),
-			'logo'			=> \dash\url::site(). '/static/images/logo.png',
-		];
 
 		$type['date'] =
 		[
 			'key'           => 'date',
+			'placeholder'   => true,
 			'title'         => T_('Date'),
-			'maxchar'       => true,
 			'logo'			=> \dash\url::site(). '/static/images/logo.png',
 		];
 
 		$type['email'] =
 		[
-			'key'           => 'email',
-			'title'         => T_('Email'),
-			'maxchar'       => true,
-			'logo'			=> \dash\url::site(). '/static/images/logo.png',
+			'key'         => 'email',
+			'placeholder' => true,
+			'title'       => T_('Email'),
+			'logo'        => \dash\url::site(). '/static/images/logo.png',
 		];
 
 		$type['website'] =
 		[
-			'key'           => 'website',
-			'title'         => T_('Website'),
-			'maxchar'       => true,
-			'logo'			=> \dash\url::site(). '/static/images/logo.png',
+			'key'         => 'website',
+			'placeholder' => true,
+			'title'       => T_('Website'),
+			'maxchar'     => true,
+			'logo'        => \dash\url::site(). '/static/images/logo.png',
 		];
 
-		// $type['rating'] =
-		// [
-		// 	'key'           => 'rating',
-		// 	'title'         => T_('Rating'),
-		// 	'maxchar'       => true,
-		// 	'logo'			=> \dash\url::site(). '/static/images/logo.png',
-		// ];
+		// card_descign
+		// rating
+		// star
+		// file_upload
 
-		// $type['star'] =
-		// [
-		// 	'key'           => 'star',
-		// 	'title'         => T_('Star'),
-		// 	'maxchar'       => true,
-		// 	'logo'			=> \dash\url::site(). '/static/images/logo.png',
-		// ];
-
-
-		// $type['file_upload'] =
-		// [
-		// 	'key'           => 'file_upload',
-		// 	'title'         => T_('File upload'),
-		// 	'logo'			=> \dash\url::site(). '/static/images/logo.png',
-		// ];
 
 		return $type;
 	}
