2e34f3fd23b33d40feb6748fe8e51bb7f40333a4
Reza <rm.biqarar@gmail.com>
2018-09-17 16:26:16 +0430

    add sarshomar log

---
+++ includes/database/upgrade/(db_name)/v.0.0.9_sarshomar_tg_log.sql
@@ -0,0 +1,7 @@
+
+ALTER TABLE `sarshomar_log`.`telegrams` ADD `send2` mediumtext CHARACTER SET utf8mb4;
+ALTER TABLE `sarshomar_log`.`telegrams` ADD `response2` mediumtext CHARACTER SET utf8mb4;
+
+ALTER TABLE `sarshomar_log`.`telegrams` ADD `send3` mediumtext CHARACTER SET utf8mb4;
+ALTER TABLE `sarshomar_log`.`telegrams` ADD `response3` mediumtext CHARACTER SET utf8mb4;
+
