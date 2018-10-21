a24bfed3cdd54a06ba2f7a8426b0d6816e9a6cd0
Reza <rm.biqarar@gmail.com>
2018-10-21 12:41:57 +0330

    show all survey list

---
+++ content_a/alllist/view.php
@@ -0,0 +1,26 @@
+<?php
+namespace content_a\alllist;
+
+
+class view
+{
+	public static function config()
+	{
+		if(!\dash\permission::supervisor())
+		{
+			\dash\header::status(403);
+		}
+
+		\dash\data::page_title(T_("Questionnaires"));
+		\dash\data::page_desc(T_("Manage all of your surveys and easily add new one or manage exisiting."));
+		\dash\data::page_pictogram('tachometer');
+
+		$arg               = [];
+		$arg['limit'] = 20;
+		$q = \dash\request::get('q');
+		$dataTable         = \lib\app\survey::list($q, $arg);
+		\dash\data::dataTable($dataTable);
+
+	}
+}
+?>
