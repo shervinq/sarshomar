b5f0cc5a58098b0efdfb3138ff36fddd62d5af55
Reza <rm.biqarar@gmail.com>
2018-10-29 20:29:01 +0330

    fix cronjob syntax

--- cronjob.php
+++ cronjob.php
@@ -1,10 +1,11 @@
 <?php
-if(file_exists('../dash/lib/engine/cronjob/run.php'))
+chdir(__DIR__);
+if(is_file('./dash/lib/engine/cronjob/run.php'))
+{
+	require_once('./dash/lib/engine/cronjob/run.php');
+}
+elseif (is_file('../dash/lib/engine/cronjob/run.php'))
 {
 	require_once('../dash/lib/engine/cronjob/run.php');
-}
-elseif (file_exists('../../dash/lib/engine/cronjob/run.php'))
-{
-	require_once('../../dash/lib/engine/cronjob/run.php');
 }
 ?>
