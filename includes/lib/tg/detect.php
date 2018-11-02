34663d3ea39f076c0823ee927948d0dbadd6534d
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-02 14:12:41 +0330

    add custom menu

--- includes/lib/tg/detect.php
+++ includes/lib/tg/detect.php
@@ -9,5 +9,45 @@ class detect
 		survey::detector($_cmd);
 	}
 
+
+	public static function mainmenu($_onlyMenu = false)
+	{
+		// define
+		$menu =
+		[
+			'keyboard' =>
+			[
+				[T_("List")],
+				[T_("About"), T_("Contact")],
+			],
+			'resize_keyboard' => true,
+		];
+
+		// add sync
+		if(\dash\user::detail('mobile'))
+		{
+			$menu['keyboard'][] = [T_("Website"). ' '. T_(\dash\option::config('site', 'title'))];
+		}
+		else
+		{
+			$menu['keyboard'][] = [T_("Sync with website")];
+		}
+
+		if($_onlyMenu)
+		{
+			return $menu;
+		}
+
+		$txt_text = T_("Main menu");
+
+		$result =
+		[
+			'text'                => $txt_text,
+			'reply_markup'        => $menu,
+		];
+
+		bot::sendMessage($result);
+		bot::ok();
+	}
 }
 ?>
