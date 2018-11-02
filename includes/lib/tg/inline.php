d4ec0514fd79df7be9db2d075f135d0c7ca45196
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-11-02 21:28:38 +0330

    add inline of sarshomar

--- includes/lib/tg/inline.php
+++ includes/lib/tg/inline.php
@@ -5,23 +5,55 @@ use \dash\social\telegram\tg as bot;
 
 class inline
 {
-	public static function search()
+	public static function search($_cmd)
 	{
-		bot::ok();
-		self::inSurveyList();
+		$surveyNo = null;
+		if(isset($_cmd['commandRaw']))
+		{
+			$surveyNo = $_cmd['commandRaw'];
+		}
+		else
+		{
+			return false;
+		}
+
+		if(!\dash\coding::is($surveyNo))
+		{
+			return false;
+		}
+
+		$welcomeDetail = \lib\app\survey::get($surveyNo);
+		$welcomeOkay   = \lib\app\survey::check($welcomeDetail);
+
+		if($welcomeOkay)
+		{
+			bot::ok();
+			self::showSurveyInline($surveyNo, $welcomeDetail);
+		}
 	}
 
 
-	public static function inSurveyList()
+	public static function showSurveyInline($_id, $_welcome)
 	{
+		$welcomeTitle = 'Survey';
+		if(isset($_welcome['welcometitle']) && $_welcome['welcometitle'])
+		{
+			$welcomeTitle = $_welcome['welcometitle'];
+		}
 
-		$siteTitle  = T_(\dash\option::config('site', 'title'));
-		$siteSlogan = T_(\dash\option::config('site', 'slogan'));
+		$welcomeDesc = 'Sarshomar';
+		if(isset($_welcome['welcomedesc']) && $_welcome['welcomedesc'])
+		{
+			$welcomeDesc = $_welcome['welcomedesc'];
+		}
 
-		$msg = "<a href='". bot::website(). "'>".$siteTitle. "</a>". "\n";
-		$msg .= $siteSlogan. "\n\n";
-		$msg .= T_(\dash\option::config('site', 'desc')). "\n";
-		$msg .= bot::website();
+		$welcomeMedia = \dash\url::site().'/static/images/logo.png';
+		if(isset($_welcome['welcomemedia']['file']) && $_welcome['welcomemedia']['file'])
+		{
+			$welcomeMedia = $_welcome['welcomemedia']['file'];
+		}
+		// get txt of survey welcome msg
+		$surveyTxt = \lib\app\tg\survey::get($_surveyId);
 
 		$resultInline =
 		[
@@ -29,31 +61,32 @@ class inline
 			[
 				[
 					'type'                  => 'article',
-					'id'                    => 1,
-					'title'                 => T_('About'). ' '. $siteTitle ,
-					'description'           => $siteSlogan,
-					'thumb_url'             =>\dash\url::site().'/static/images/logo.png',
+					'id'                    => 1001,
+					'title'                 => $welcomeTitle,
+					'description'           => $welcomeDesc,
+					'thumb_url'             => $welcomeMedia,
 					'input_message_content' =>
 					[
-						'message_text' => $msg,
+						'message_text' => $surveyTxt,
 						'parse_mode'   => 'html'
 					],
+
 					'reply_markup' =>
 					[
 						'inline_keyboard' =>
 						[
 							[
 								[
-									'text' => T_("Open :val website", ['val' => $siteTitle]),
-									'url'  => bot::website(),
+									'text' => T_("Answer via site"),
+									'url'  => \dash\url::base(). '/s/'. $_id,
 								],
 							],
 							[
 								[
-									'text' => T_(":val Telegram bot", ['val' => $siteTitle]),
-									'url'  => 'https://t.me/'. bot::$name,
+									'text'          => 	T_("Answer via bot"),
+									'callback_data' => 'survey_'. $_id. ' start',
 								],
-							]
+							],
 						]
 					],
 				]
