284c9941c0ac3600b19a7de7d7b86e6719727655
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-11-03 02:31:11 +0330

    add hafez sample

---
+++ includes/lib/tg/hafez.php
@@ -0,0 +1,19 @@
+<?php
+namespace lib\tg;
+use \dash\social\telegram\tg as bot;
+
+
+class hafez
+{
+	public static function niyat()
+	{
+
+	}
+
+	// api1 sample http://emrani.net/hafez/api/hafez/fal
+	public static function fal()
+	{
+
+	}
+}
+?>
