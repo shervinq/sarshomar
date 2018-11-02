8e9413cc0595523a7d6b753a2b269c1215c4b346
Javad Kakroudi <J.Evazzadeh@gmail.com>
2018-11-03 03:07:22 +0330

    add hafez fn

--- includes/lib/tg/hafez.php
+++ includes/lib/tg/hafez.php
@@ -5,15 +5,144 @@ use \dash\social\telegram\tg as bot;
 
 class hafez
 {
-	public static function niyat()
+	public static function check($_cmd)
 	{
+		switch ($_cmd['command'])
+		{
+			case '/fal':
+			case 'fal':
+			case '/hafez':
+			case 'hafez':
+			case 'فال':
+			case 'فالگیر':
+			case 'فال بگیر':
+			case 'فال حافظ':
+			case 'حافظ':
+			case 'تفعل':
+			case 'خواجه':
+			case 'بگو ای خواجه':
+			case 'خواجه شیرازی':
+			case 'ای حافظ شیرازی':
+				self::niyat();
+				return true;
+				break;
 
+
+			case 'cb_hafezSaySomething':
+				// if start with callback answer callback
+				if(bot::isCallback())
+				{
+					self::fal();
+				}
+				break;
+
+
+			case 'cb_hafezReadIt':
+				// if start with callback answer callback
+				if(bot::isCallback())
+				{
+					self::falReader();
+				}
+				break;
+
+		}
+
+		return false;
 	}
 
-	// api1 sample http://emrani.net/hafez/api/hafez/fal
-	public static function fal()
-	{
 
+	private static function niyat()
+	{
+		bot::ok();
+
+		$text .= '<b>فال #حافظ</b>'. "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+
+		$result =
+		[
+			'text' => $text,
+			'reply_markup' =>
+			[
+				'inline_keyboard' =>
+				[
+					[
+						[
+							'text' => T_("Lets go"),
+							'callback_data' => 'hafezSaySomething',
+						],
+					]
+				]
+			]
+		];
+		bot::sendMessage($result);
 	}
+
+
+	private static function fal()
+	{
+		bot::ok();
+		bot::answerCallbackQuery(T_("Fal of Hafez"));
+
+		// get random fal from hafez
+
+		$text .= T_('Soon'). "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+
+		$result =
+		[
+			'text' => $text,
+			'reply_markup' =>
+			[
+				'inline_keyboard' =>
+				[
+					[
+						[
+							'text' => T_("Lets go"),
+							'callback_data' => 'hafezReadIt 12',
+						],
+					]
+				]
+			]
+		];
+		bot::sendMessage($result);
+	}
+
+
+
+	private static function falReader()
+	{
+		bot::ok();
+		bot::answerCallbackQuery(T_("Fal of Hafez"). ' 🎻');
+
+		// get random fal from hafez
+
+		$text .= T_('Soon'). "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+		$text .= ''. "\n";
+
+		$result =
+		[
+			'text' => $text,
+			'reply_markup' =>
+			[
+				'inline_keyboard' =>
+				[
+					[
+						[
+							'text' => T_("Open :val website", ['val' => T_(\dash\option::config('site', 'title'))]),
+							'url'  => bot::website(),
+						],
+					]
+				]
+			]
+		];
+		bot::sendMessage($result);
+	}
+
 }
 ?>
