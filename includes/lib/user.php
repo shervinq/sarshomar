a6aee0f6ba7a892547853524a4a46b392ad8c9c2
Reza <rm.biqarar@gmail.com>
2019-01-22 17:25:20 +0330

    remove question

--- includes/lib/user.php
+++ includes/lib/user.php
@@ -11,7 +11,7 @@ class user
 		$result['answerdetails'] = \lib\db\answerdetails::get_count(['user_id' => $_user_id]);
 		$result['answers']       = \lib\db\answers::get_count(['user_id' => $_user_id]);
 		$result['answerterms']   = \lib\db\answerterms::get_count(['user_id' => $_user_id]);
-		$result['questions']     = \lib\db\questions::get_count(['user_id' => $_user_id]);
+		// $result['questions']     = \lib\db\questions::get_count(['user_id' => $_user_id]);
 		$result['surveys']       = \lib\db\surveys::get_count(['user_id' => $_user_id]);
 
 		return $result;
