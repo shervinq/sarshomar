e13c5359c9c661691d8f755602137405183f950e
Reza <rm.biqarar@gmail.com>
2019-01-25 14:51:41 +0330

    set back link

--- content_a/report/wordcloud/view.php
+++ content_a/report/wordcloud/view.php
@@ -10,6 +10,9 @@ class view
 		\dash\data::page_title(T_("Word cloud"));
 		\dash\data::page_desc(T_("List of your survey word"));
 
+		\dash\data::badge_link(\dash\url::this(). '?id='. \dash\request::get('id'));
+		\dash\data::badge_text(T_('Back to report dashboard'));
+
 		\dash\data::allWordCloud(\lib\app\survey::word_cloud(\dash\request::get('id')));
 
 	}
