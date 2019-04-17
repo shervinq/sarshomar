a3040feaff1d50548b4b284b6b1816b1da074735
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-04-17 10:34:17 +0430

    add review page

---
+++ content_a/survey/review/view.php
@@ -0,0 +1,46 @@
+<?php
+namespace content_a\survey\review;
+
+
+class view
+{
+	public static function config()
+	{
+		\dash\data::page_title(T_("Review Survey"));
+		\dash\data::page_desc(T_("Check your survey detail and track everything about this survey."));
+		\dash\data::page_pictogram('plus-zoom');
+
+		if(\dash\request::get('id'))
+		{
+			$id          = \dash\request::get('id');
+			$load_survey = \lib\app\survey::get($id);
+			if(!$load_survey)
+			{
+				\dash\header::status(404, T_("Invalid survey id"));
+			}
+			\dash\data::surveyRow($load_survey);
+
+			\dash\data::page_title(\dash\data::page_title(). ' | '. \dash\data::surveyRow_title());
+
+			\dash\data::badge_link(\dash\url::kingdom(). '/s/'. $id);
+			\dash\data::badge_text(T_('Preview'));
+
+			\dash\data::badge2_link(\dash\url::here());
+			\dash\data::badge2_text(T_('Back to survey list'));
+
+			$dashboard_detail['month_detail'] = \dash\date::month_precent();
+
+			\dash\data::dashboardDetail($dashboard_detail);
+
+
+			$questionsData = \lib\app\question::block_survey($id);
+			\dash\data::questionList($questionsData);
+		}
+		else
+		{
+			\dash\redirect::to(\dash\url::here());
+		}
+
+	}
+}
+?>
