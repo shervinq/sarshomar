a60d2e7b94aa99db4b9d72150d321c072026a3fd
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-05-12 08:42:29 +0430

    send sample photo

--- includes/lib/tg/inline.php
+++ includes/lib/tg/inline.php
@@ -60,47 +60,65 @@ class inline
 		// get txt of survey welcome msg
 		$surveyTxt = \lib\app\tg\survey::get($_id);
 
-		$resultInline =
+		// $resultInline =
+		// [
+		// 	'inline_query_id' => hook::inline_query('id'),
+		// 	'results' =>
+		// 	[
+		// 		[
+		// 			'type'                  => 'article',
+		// 			'id'                    => '1',
+		// 			'title'                 => $surveyTitle,
+		// 			// 'description'           => $surveyDesc,
+		// 			// 'thumb_url'             => $welcomeMedia,
+		// 			// 'cache_time'            => 60,
+		// 			'input_message_content' =>
+		// 			[
+		// 				'message_text' => $surveyTxt,
+		// 				'parse_mode'   => 'html'
+		// 			],
+
+		// 			// 'reply_markup' =>
+		// 			// [
+		// 			// 	'inline_keyboard' =>
+		// 			// 	[
+		// 			// 		[
+		// 			// 			[
+		// 			// 				'text' => T_("Answer via site"),
+		// 			// 				'url'  => \dash\url::base(). '/s/'. $_id,
+		// 			// 			],
+		// 			// 		],
+		// 			// 		[
+		// 			// 			[
+		// 			// 				'text' => 	T_("Answer via bot"),
+		// 			// 				'url'  => bot::deepLink('survey_'. $_id)
+		// 			// 			],
+		// 			// 		],
+		// 			// 	]
+		// 			// ],
+		// 		]
+		// 	]
+		// ];
+
+		// bot::answerInlineQuery($resultInline);
+
+
+
+		$resultInlineTest =
 		[
-			'inline_query_id' => hook::inline_query('id'),
 			'results' =>
 			[
 				[
-					'type'                  => 'article',
-					'id'                    => '1',
-					'title'                 => $surveyTitle,
-					// 'description'           => $surveyDesc,
-					// 'thumb_url'             => $welcomeMedia,
-					// 'cache_time'            => 60,
-					'input_message_content' =>
-					[
-						'message_text' => $surveyTxt,
-						'parse_mode'   => 'html'
-					],
-
-					// 'reply_markup' =>
-					// [
-					// 	'inline_keyboard' =>
-					// 	[
-					// 		[
-					// 			[
-					// 				'text' => T_("Answer via site"),
-					// 				'url'  => \dash\url::base(). '/s/'. $_id,
-					// 			],
-					// 		],
-					// 		[
-					// 			[
-					// 				'text' => 	T_("Answer via bot"),
-					// 				'url'  => bot::deepLink('survey_'. $_id)
-					// 			],
-					// 		],
-					// 	]
-					// ],
+					'type'                  => 'photo',
+					'id'                    => '2',
+					'photo_url'             => $welcomeMedia,
+					'thumb_url'             => $welcomeMedia,
 				]
 			]
 		];
 
-		bot::answerInlineQuery($resultInline);
+		bot::InlineQueryResultPhoto($resultInlineTest);
+
 
 	}
 }
